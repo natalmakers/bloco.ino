be2e2ae487cb398c93f2b436ebb7a559666ae8e9
NatalMakers <natalmakers@gmail.com>
2019-11-14 17:48:57 -0300

    Add files via upload

---
+++ examples/movimento_pendular_bloco.ino
@@ -0,0 +1,9 @@
+#include "blocolib.h"
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  ativarRadar();
+
+}
---
+++ examples/solubilizador_bloco.ino
@@ -0,0 +1,11 @@
+#include "blocolib.h"
+void setup() {
+  Serial.begin(9600);
+
+}
+
+void loop() {
+  
+  misturador(A0,9);
+
+}
